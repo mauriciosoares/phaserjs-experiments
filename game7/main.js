2083b82f08f2fa69454b54baffb5458acd6cae4a
Mauricio Soares <m.soares2603@gmail.com>
2014-11-17 17:27:24 -0200

    improvements on following algorithm

--- game7/main.js
+++ game7/main.js
@@ -14,7 +14,7 @@ GameState.prototype.create = function() {
   var NUMBER_OF_FOLLOWERS = 10;
   for(var i = 0; i < NUMBER_OF_FOLLOWERS; i += 1) {
     var f = this.game.add.existing(
-      new Follower(this.game, this.game.width / 2, this.game.height / 2, f || this.game.input)
+      new Follower(this.game, this.game.width / 2 + i * 32, this.game.height / 2, f || this.game.input)
     );
   }
 
@@ -49,13 +49,36 @@ Follower.prototype = Object.create(Phaser.Sprite.prototype);
 Follower.prototype.constructor = Follower;
 
 Follower.prototype.update = function() {
-  var distance = this.game.math.distance(this.x, this.y, this.target.x, this.target.y);
+  var t = {};
+  var targetMoving = false;
 
-  if(distance > this.MIN_DISTANCE) {
-    var rotation = this.game.math.angleBetween(this.x, this.y, this.target.x, this.target.y);
+  if(this.target.history && this.target.history.length) {
+    // target has history, so go towards that
+    t = this.target.history[0];
+    if(this.target.body.velocity.x || this.target.body.velocity.y) targetMoving = true;
+  } else {
+    // doesnt have history, just follow x and y
+    t.x = this.target.x;
+    t.y = this.target.y;
 
+    var distance = this.game.math.distance(this.x, this.y, this.target.x, this.target.y);
+    if(distance > this.MIN_DISTANCE) targetMoving = true;
+  }
+
+
+  if(targetMoving) {
+    // add current position to end of history array
+    this.history.push({ x: this.x, y: this.y });
+
+    if(this.history.length > this.HISTORY_LENGTH) this.history.shift();
+
+    // Calculate the angle to the target
+    var rotation = this.game.math.angleBetween(this.x, this.y, t.x, t.y);
+
+    // Calculate velocity vector based on rotation and this.MAX_SPEED
     this.body.velocity.x = Math.cos(rotation) * this.MAX_SPEED;
     this.body.velocity.y = Math.sin(rotation) * this.MAX_SPEED;
+
   } else {
     this.body.velocity.setTo(0, 0);
   }
